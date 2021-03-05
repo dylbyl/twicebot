7b37583a0dffad1773c36fdf31e4d7d819e35d99
daltonrussell <dalton.russell1000@gmail.com>
2021-03-04 20:59:28 -0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -24,7 +24,31 @@ gifs = ['https://media1.giphy.com/media/W5TEa73iw1fnSVjjZZ/200w.webp?cid=ecf05e4
         'https://media0.giphy.com/media/ns4ICutK9rKu1Em3md/200w.webp?cid=ecf05e47giypmn6cvzaat4x2149xjhcmk1itrvmporudytwh&rid=200w.webp',
         'https://media1.giphy.com/media/QvqnK8LfwTEnatCyTt/200w.webp?cid=ecf05e47giypmn6cvzaat4x2149xjhcmk1itrvmporudytwh&rid=200w.webp',
         'https://media0.giphy.com/media/YMN3au52fbXi44lI81/200.webp?cid=ecf05e47o4u7s4t7qii44yq4e47dcbznh8dcd6rowjj7wh27&rid=200.webp',
-        'https://media2.giphy.com/media/W02KJMd0xRj21zeq3K/200w.webp?cid=ecf05e478sq4now4fkgouv6omqrjsydqdv9yhy01ss0o5y8c&rid=200w.webp']
+        'https://media2.giphy.com/media/W02KJMd0xRj21zeq3K/200w.webp?cid=ecf05e478sq4now4fkgouv6omqrjsydqdv9yhy01ss0o5y8c&rid=200w.webp',
+        'https://media.giphy.com/media/eLXadZuZ1hN8AOclZR/giphy.gif',
+        'https://media.tenor.com/images/bb5ae136c9dc3de7b9757cf2a156b460/tenor.gif',
+        'https://media.giphy.com/media/kEoguATlRXPLHRTaY8/giphy.gif',
+        'https://media.giphy.com/media/xLI6yofqY6hzO/giphy.gif',
+        'https://media.giphy.com/media/l1Asyt8E4bw5zAZS8/giphy.gif',
+        'https://media.giphy.com/media/3ofT5YwTXNXZJDlS5a/giphy.gif',
+        'https://media.giphy.com/media/3ofT5YKxH7fS4vsNQk/giphy.gif',
+        'https://media.giphy.com/media/hqm43RkgSjWin0KkO3/giphy.gif',
+        'https://media.giphy.com/media/U6e8oEzVPH8j01UUiD/giphy.gif',
+        'https://media.giphy.com/media/Qrcs9WG2ZlPvOdNSqO/giphy.gif',
+        'https://media.giphy.com/media/lmuX8VzZqm2Ostyh7b/giphy.gif',
+        'https://media.giphy.com/media/xUySTtPKh79lZVQ93i/giphy.gif',
+        'https://media.giphy.com/media/hWGi1kHpWycSSpfTmu/giphy.gif',
+        'https://media.giphy.com/media/3ofT5VgD5NOgKbYlna/giphy.gif',
+        'https://media.giphy.com/media/JsQX89yt6toWPFHrb8/giphy.gif',
+        'https://media.giphy.com/media/hV6z5HJfJ14VjGKwSt/giphy.gif',
+        'https://media.giphy.com/media/Kb5AssJPAutnvwwpbV/giphy.gif',
+        'https://media.giphy.com/media/xUySTXCrUBCNEA5lGE/giphy.gif',
+        'https://media.giphy.com/media/UWmerXS1W8h7CML7tG/giphy.gif',
+        'https://media.giphy.com/media/JsKdu4ikYSoxaAhJOB/giphy.gif',
+        'https://media.giphy.com/media/TIFCfyNwKlus4oGzuk/giphy.gif',
+        'https://media.giphy.com/media/c8En1CeEuqRri/giphy.gif',
+        'https://media.giphy.com/media/eLXadZuZ1hN8AOclZR/giphy.gif',
+        'https://media.giphy.com/media/eMgVjuhiwq5USE3icW/giphy.gif',]
 
 client = discord.Client()
 
